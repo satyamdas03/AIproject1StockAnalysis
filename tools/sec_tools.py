5de6d56c3b069fe38f6fba635383bd5ec6ac653c
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-07-26 18:22:46 +0530

    sec tools coding up

--- tools/sec_tools.py
+++ tools/sec_tools.py
@@ -1,1 +1,8 @@
 # sec_tools 
+import os
+import requests
+from langchain.tools import tool
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
